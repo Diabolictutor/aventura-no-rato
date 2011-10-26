fbf456d9f3a73dc86515d98986ebf6799bc9ae4d
Sérgio Lopes <knitter.is@gmail.com>
2011-10-26 20:04:22 +0100

    Added error controller and view.

---
+++ anr/core/Error.php
@@ -0,0 +1,32 @@
+<?php
+
+/* Error.php
+ * 
+ * This file is part of Aventura no Rato! A browser based, adventure type, game.
+ * Copyright (C) 2011  Diogo Samuel, Jorge Gonçalves, Pedro Pires e Sérgio Lopes
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+class Forum extends Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $this->render('site/error');
+    }
+
+}
