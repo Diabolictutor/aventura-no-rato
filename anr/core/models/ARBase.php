b370ad79b7b27a4765e3792ddd33f321de6242f8
Sérgio Lopes <knitter.is@gmail.com>
2011-11-02 20:26:18 +0000

    Small corrections and updates to prepare work on server side code.

--- anr/core/models/ARBase.php
+++ anr/core/models/ARBase.php
@@ -1,14 +1,63 @@
 <?php
 
+/* ARBase.php
+ * 
+ * This file is part of Aventura no Rato! A browser based, adventure type, game.
+ * Copyright (C) 2011  Diogo Samuel, Jorge Gonçalves, Pedro Pires e Sérgio Lopes
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+/**
+ * Generic abstract class that is the base for all <em>Active Record</em> classes.
+ * This class provides only limited basic features as an example and common ground 
+ * for creating all other model classes.
+ */
 abstract class ARBase {
-    
-    private $scenario;
-    
-    public function __construct($scenario = 'insert') {
-        $this->scenario = $scenario;
+
+    /**
+     * Flag that indicates if the class represents a new record, not 
+     * yet stored in the database. 
+     * 
+     * @var integer
+     */
+    public $newRecord;
+
+    /**
+     * The name of the database table that relates to this model. It 
+     * should be filled automatically but can be changed after the model is created. 
+     * 
+     * @var string
+     */
+    public $table;
+
+    public function __construct($newRecord = true) {
+        $this->newRecord = $newRecord;
+        $this->table = get_class();
     }
 
-    private function connect() {
+    /**
+     * Creates a database connection to be used in subsequent database calls.
+     * 
+     * @global string $serverName
+     * @global string $dbUser
+     * @global string $dbName
+     * @global string $dbPass
+     * 
+     * @return boolean True if the connection was created, false otherwise.
+     */
+    public function connect() {
         global $serverName, $dbUser, $dbName, $dbPass;
 
         if (!mysql_connect($serverName, $dbUser, $dbPass)) {
@@ -17,13 +66,68 @@ abstract class ARBase {
 
         return mysql_select_db($dbName);
     }
-    
-    public function save() {
-        
-    }
-    
-    public function find() {
-        
+
+    /**
+     * Closes an existing database connection.
+     */
+    public function disconnect() {
+        mysql_close();
     }
 
+    /**
+     *
+     * @param string $criteria
+     * @param string $field
+     * 
+     * @return integer The number of records found.
+     */
+    public function count($criteria = '', $field = '*') {
+        $count = 0;
+
+        $where = '';
+        if ($criteria != '') {
+            $where = 'WHERE ' . $criteria;
+        }
+        $query = sprintf("SELECT COUNT( %s ) AS 'number' FROM %s %s", $field, $this->table, $where);
+
+        if ($this->connect()) {
+            if (($resource = mysql_query($query)) && mysql_affected_rows() > 0) {
+                $count = mysql_fetch_object($resource)->number;
+                mysql_free_result($resource);
+            }
+            $this->disconnect();
+        }
+
+        return $count;
+    }
+
+    /**
+     *
+     * @param string $criteria
+     * 
+     * @return boolean True if records were removed, false otherwise 
+     */
+    public function delete($criteria) {
+        $success = false;
+
+        $query = sprintf("DELETE FROM %s WHERE %s LIMIT 1", $this->table, $where);
+        if ($this->connect()) {
+            $success = (mysql_query($query) && mysql_affected_rows());
+            $this->disconnect();
+        }
+
+        return $success;
+    }
+
+    public abstract function findByPk($key, $fields = '*');
+
+    public abstract function find($criteria = '', $fields = '*');
+
+    public abstract function findAll($criteria = '', $fields = '*');
+
+    public abstract function save();
+
+    public abstract function refresh();
+
+    public abstract static function model();
 }
--- anr/core/models/User.php
+++ anr/core/models/User.php
@@ -1,111 +1,128 @@
 <?php
 
-class User {
+/* User.php
+ * 
+ * This file is part of Aventura no Rato! A browser based, adventure type, game.
+ * Copyright (C) 2011  Diogo Samuel, Jorge Gonçalves, Pedro Pires e Sérgio Lopes
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
 
-    private $userID;
-    private $email;
-    private $password;
-    private $name;
-    private $group;
-    private $signature;
-    private $website;
-    private $lastLogin;
-    private $active;
-    private $avatar;
-    private $postPerPage;
-    private $newRecord;
+/**
+ * @property $userID
+ * @property $email;
+ * @property $password;
+ * @property $name;
+ * @property $group;
+ * @property $signature;
+ * @property $website;
+ * @property $lastLogin;
+ * @property $active;
+ * @property $avatar;
+ */
+class User extends ARBase {
 
-    public function __construct($newRecord = true) {
-        $this->newRecord = $newRecord;
-    }
+    public $userID;
+    public $email;
+    public $password;
+    public $name;
+    public $group;
+    public $signature;
+    public $website;
+    public $lastLogin;
+    public $active;
+    public $avatar;
+    public $postPerPage;
 
-    private function connect() {
-        global $serverName, $dbUser, $dbName, $dbPass;
-
-        if (!mysql_connect($serverName, $dbUser, $dbPass)) {
-            return false;
-        }
-
-        return mysql_select_db($dbName);
+    public function __construct() {
+        parent::__construct();
     }
 
     public function save() {
-
         if ($this->newRecord) {
-
-            $this->connect();
-            mysql_query(sprintf("INSERT INTO User (
+            $insert = sprintf("
+                INSERT INTO `User` (
                     `email`, 
                     `password`,
                     `name`,
                     `group`,
                     `signature`,
                     `website`,
-                    `lastLogin`,
-                    `active`,
                     `avatar`,
                     `postPerPage`) 
-                    VALUES ('%s', '%s', '%s', %d, '%s', '%s', '%s', %d, '%s')", 
-                    $this->email, 
-                    $this->password, 
-                    $this->name, 
-                    $this->group, 
-                    $this->signature, 
-                    $this->website, 
-                    $this->lastLogin, 
-                    $this->active, 
-                    $this->avatar));
-            $this->userID=mysql_insert_id();
-            mysql_close();
+                VALUES ('%s', '%s', '%s', %d, '%s', '%s', '%s', %d)"
+                    , $this->email, $this->password, $this->name, $this->group
+                    , $this->signature, $this->website, $this->avatar, $this->postPerPage);
+
+            if ($this->connect()) {
+                $this->disconnect();
+                if (mysql_query($insert) && mysql_affected_rows() == 1) {
+                    $this->userID = mysql_insert_id();
+                    return true;
+                }
+            }
         } else {
-            $this->connect();
-            mysql_query(sprintf("UPDATE User SET 
+            $update = sprintf("
+                UPDATE `User` SET 
                     `email` = '%s', 
                     `password` = '%s',
                     `name` = '%s',
                     `group` = %d,
                     `signature` = '%s',
                     `website` = '%s',
-                    `lastLogin` = '%s',
                     `active` = %d,
                     `avatar` = '%s',
-                    `postPerPage` = '%s') 
-                    )", 
-                    $this->email, 
-                    $this->password, 
-                    $this->name, 
-                    $this->group, 
-                    $this->signature, 
-                    $this->website, 
-                    $this->lastLogin, 
-                    $this->active, 
-                    $this->avatar));
-            mysql_close();
+                    `postPerPage` = %d
+                WHERE `userID` = %d"
+                    , $this->email, $this->password, $this->name, $this->group
+                    , $this->signature, $this->website, $this->lastLogin, $this->active
+                    , $this->avatar, $this->userID
+            );
+
+            if ($this->connect()) {
+                if (mysql_query($update) && mysql_affected_rows() == 1) {
+                    return true;
+                }
+                $this->disconnect();
+            }
         }
+
+        return false;
     }
 
-    public function find() {
+    public function find($criteria = '', $fields = '*') {
         
     }
 
-    public function findAll() {
+    public function findAll($criteria = '', $fields = '*') {
         
     }
 
-    public function findByPk() {
+    public function findByPk($key, $fields = '*') {
         
     }
 
-    public function count() {
+    public function refresh() {
         
     }
 
-    public function delete() {
-        
-    }
-
-    public function deleteAll() {
-        
+    /**
+     *
+     * @return User 
+     */
+    public static function model() {
+        return new User();
     }
 
 }
