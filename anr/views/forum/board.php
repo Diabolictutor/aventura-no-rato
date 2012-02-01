7e28e5e1db5f0cb02921a01baa4e91b928a38bd8
Jorge Gonçalves <jorgemdgoncalves@hotmail.com>
2012-02-01 19:05:12 +0000

    boards and stuff

--- anr/views/forum/board.php
+++ anr/views/forum/board.php
@@ -1,19 +1,14 @@
 <div>
     <div>
-        <h2 style="background-color:darkgrey;float:left;width:100%;">TOPIC 1</h2>
+        <h2>Board Name</h2>
     </div>
     <div style="clear:both;"></div>
-    <div style="background-color:lightgrey;width:100%">
+    <div class="board">
         <ul>
-            <li>Thread 1 -- "Thread Title" by "USERNAME" on "date"</li>
-            <li>Thread 2 -- "Thread Title" by "USERNAME" on "date"</li>
-            <li>Thread 3 -- "Thread Title" by "USERNAME" on "date"</li>
-            <li>Thread 4 -- "Thread Title" by "USERNAME" on "date"</li>
-            <li>Thread 5 -- "Thread Title" by "USERNAME" on "date"</li>
-            <li>Thread 6 -- "Thread Title" by "USERNAME" on "date"</li>
-            <li>Thread 7 -- "Thread Title" by "USERNAME" on "date"</li>
-            <li>Thread 8 -- "Thread Title" by "USERNAME" on "date"</li>
-            <li>Thread 9 -- "Thread Title" by "USERNAME" on "date"</li>
+            <li><a href="<?php echo $this->createUrl(); ?>">
+                    "<?php echo $thread->title;?>" 
+                    by "<?php echo $thread->author;?>" 
+                    on "<?php echo $thread->data;?>"</a></li>
         </ul>
     </div>
 </div>
