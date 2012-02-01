298a77e978c19352dce2dec4634d94188d235394
Diogo <diogo@diogo-jdr.(none)>
2012-02-01 19:04:49 +0000

    Boards CSS Implementation

--- anr/views/administration/boards.php
+++ anr/views/administration/boards.php
@@ -1,12 +1,15 @@
 <?php echo $this->includeViewFile('administration/_admin-menu'); ?>
 <div id="board-list">
-    <!-- //TODO: Proper icon -->
-    <a href="<?php echo $this->createURL(array('c' => 'administration', 'a' => 'editboard')); ?>">Inserir</a>
+    <a href="<?php echo $this->createURL(array('c' => 'administration', 'a' => 'editboard')); ?>"
+       class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only">
+        Add
+    </a>
     <ul>
         <?php foreach ($this->boards as $board) { ?>
             <li>
-                <a href="<?php echo $this->createURL(array('c' => 'administration', 'a' => 'editboard'), array('id' => $board->boardID)); ?>" alt="">
-                    <?php echo $board->title; ?>
+                <a href="<?php echo $this->createURL(array('c' => 'administration', 'a' => 'editboard'), array('id' => $board->boardID)); ?>"
+                   class="">
+                       <?php echo $board->title; ?>
                 </a>
             </li>
         <?php } ?>
